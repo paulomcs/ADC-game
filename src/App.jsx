32370e08170dc3140356555aff7d862330426b18
paulomcs <paulocunhasampaio@gmail.com>
2024-06-17 02:05:42 -0300

    bg images addition

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,26 @@ import HelpButton from "./components/HelpButton";
 import SearchBar from "./components/SearchBar";
 function App() {
   return (
-    <>
+    <div className="w-full h-full bg-[url('/src/assets/background.png')]">
       <Title />
       <div className="guess w-96 text-center text-black text-4xl font-semibold font-['Poppins'] leading-10">
         Adivinhe o personagem de adventures of ccpa de hoje!
         <br />
       </div>
-      <div className="helpNav flex">
-        <HelpButton />
-        <HelpButton />
-        <HelpButton />
+
+
+      <div className="">
+        <div className="Rectangle36 w-96 h-10 bg-black border border-amber-300" />
+        <div className="helpNav flex">
+          <HelpButton />
+          <HelpButton />
+          <HelpButton />
+        </div>
       </div>
+      
+      
       <SearchBar />
-    </>
+    </div>
   );
 }
 
